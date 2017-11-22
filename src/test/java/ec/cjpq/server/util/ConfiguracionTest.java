db9e593088e50dcf672fa3c91ff4c43664c03f79
Knoppix User <carlosjulioperez@gmail.com>
2017-11-22 05:06:55 -0500

    Test

---
+++ src/test/java/ec/cjpq/server/util/ConfiguracionTest.java
@@ -0,0 +1,17 @@
+package ec.cjpq.server.rest.util;
+
+import java.util.Map;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class ConfiguracionTest{
+
+    @Test
+    public void testNewConfiguracion(){
+        
+        Map values = Configuracion.toMap();
+        System.out.println(values.get("dbDriver"));
+
+    }
+}
