3a0952b56819f89444e59f12180c8d4a30822fd4
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2017-12-01 09:19:35 -0500

    Script para datos_contenedor

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS datos_contenedor;
 DROP TABLE IF EXISTS inspeccion;
 
 CREATE TABLE inspeccion(
@@ -29,5 +30,50 @@ CREATE TABLE inspeccion(
     chasis_estado   boolean,
     observaciones   varchar(100),
     inspector       varchar(20)
+);
+
+CREATE TABLE datos_contenedor(
+    id SERIAL PRIMARY KEY,
+    contenedor_id       integer REFERENCES inspeccion,
+    lugar_inspeccion    varchar(100),
+    oin                 varchar(100),
+    consignatario       varchar(100),
+    cantidad_cajas      varchar(100),
+    sello_sgs           varchar(20),
+    otro_sello          varchar(20),
+    sello_agencia       varchar(20),
+    sello_exportador    varchar(20),
+    preenfriado         boolean, --condicion contenedor
+    drenaje             boolean,
+    probado             boolean,
+    sc_deteriorada      boolean,
+    olor_extrano        boolean,
+    marca_equipo        varchar(20),
+    escarchados         smallint, --cartones
+    mojados             smallint,
+    rotos               smallint,
+    manchados           smallint,
+    marca_cajeta        varchar(20),
+    marca_master        varchar(20),
+    temperatura1        numeric(4,2), --99.99
+    temperatura2        numeric(4,2), --99.99
+    temperatura3        numeric(4,2), --99.99
+    temperatura4        numeric(4,2), --99.99
+    temperatura5        numeric(4,2), --99.99
+    temperatura6        numeric(4,2), --99.99
+    temperatura7        numeric(4,2), --99.99
+    temperatura8        numeric(4,2), --99.99
+    temperatura9        numeric(4,2), --99.99
+    temperatura10       numeric(4,2), --99.99
+    temperatura_prom    numeric(4,2), --99.99
+    embalaje            varchar(200),
+    estiba              varchar(100),
+    tipo_estiba         char(1),
+    temp_antes_estiba   boolean,
+    temp_despues_estiba boolean,
+    numero_ryan         varchar(20),
+    numero_sello_ryan   varchar(20),
+    numero_grafico      varchar(20),
+    cartilla            boolean
 );
 
